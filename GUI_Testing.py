fd78edd690c69803b50298745c1b2d0675cb524c
Calvin Teuber <calvinteuber7@gmail.com>
2022-11-15 14:20:34 +0100

    FaceTracking

--- GUI_Testing.py
+++ GUI_Testing.py
@@ -22,13 +22,12 @@ if __name__ == "__main__":
 
     # Testing case to enable and disable the cameras
     cam_state = True
-    hand_track_flag = True
 
-    drone_state = False
+    drone_state = True
     d_cam_state = True
 
     # setting the width and height for the Webcam
-    width, height = 800, 600
+    width, height = 1920, 1080
 
     if cam_state == True:
         print('Init cap...')
@@ -36,9 +35,6 @@ if __name__ == "__main__":
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
         print('Cap initialized!')
-
-        if hand_track_flag == True:
-            start.hand_track(lmain, cap)
 
     if drone_state == True:
         tello.connect()
@@ -55,7 +51,7 @@ if __name__ == "__main__":
 
     cont_var = StringVar(root, "0")
     dcam_var = StringVar(root, "0")
-    throt_var = IntVar(root, 100)
+    throt_var = IntVar(root, 70)
 
 
     start.buttons(cont_var,throt_var, lmain, root, dcam_var, tello) 
@@ -70,10 +66,11 @@ if __name__ == "__main__":
             start.total_annihilation(dcam_var, tello,  root)
 
         if cam_state == True:               # If the cam has been enabled hand tracking will also start, not sure if this can be implemented to only start if enabled in the GUI
-            start.hand_track(lmain, cap)    #type: ignore
+            start.Cam(lmain, cap)    #type: ignore
        
         cam_stream = dcam_var.get()
         controller = cont_var.get()
+        throttle = throt_var.get()
 
         if controller == "1":               # Enters Xbox Controll Mode
             joy.flight_xbox(tello)
@@ -104,19 +101,9 @@ if __name__ == "__main__":
                 face.tk_facetrack(tello, 20, face_cascade)  #type: ignore
 
         elif controller == "4":             #Is the Gesture Tracking
-            if drone_state == False:
-                print("turning the drone stream on...")
-                tello.streamoff()
-                tello.streamon()
-                print("Stream turned on")
 
-                drone_state = True
-            else:               
-                drone_cam = tello.get_frame_read().frame
-
-                if flight_flag == False:
-                    tello.takeoff()
-                    flight_flag = True
+            hand.tk_handflight(tello, cap, throttle)
+                
 
         if cam_stream == "1":
             start.drone_stream(tello)
