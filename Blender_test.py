76d78095867ec766c0c3a77fb1df0a311088548f
Calvin Teuber <calvinteuber7@gmail.com>
2022-11-30 15:10:33 +0100

    Started development on the Blender integration

--- Blender_test.py
+++ Blender_test.py
@@ -1,2 +1,21 @@
+from time import sleep
+
 import bpy
+blender = bpy
+
+def set_location(name, xyz):
+    bpy.data.objects[name].location = xyz
+
+def set_euler_rotation(name, xyz):
+    bpy.data.objects[name].rotation_euler = xyz
+
+def set_scale(name, xyz):
+    bpy.data.objects[name].scale = xyz
+
+x = 3.1415 * 0.5 
+y = 3.1415 * 0.5
+z = 3.1415 * 0.5
+
+for obj in bpy.data.objects:
+    print(obj.name)
 
